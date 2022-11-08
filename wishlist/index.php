0ede6cba640a9c44c125d5e504ec810c1f6f200f
erm26 <57805783+erm26@users.noreply.github.com>
2022-11-08 20:50:14 +0600

    Add files via upload

---
+++ wishlist/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<form action="wishList.php" method="get">
+		Show wishlist of: 
+		<input type="text" name="user">
+		<input type="submit" value="Go">
+		<br>Still don't have a wish list?! <a href="newWisher.php">Create now</a>
+	</form>
+</body>
+</html>
